c9ac89d2cb5a0cc9b55c6020ceffe16c105ae964
Arman <mdarman365506@gmail.com>
2025-08-05 12:50:19 +0600

    chore: initialize project with navbar and footer layout

---
+++ src/components/modules/about/About.tsx
@@ -0,0 +1,10 @@
+
+
+export default function About() {
+  return (
+      <div>
+          <h1>This is About Component</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt corrupti expedita optio temporibus, doloremque minima quas maxime cupiditate corporis magni? Placeat, quidem impedit odio omnis dolorum eveniet neque quasi sint voluptas id rerum est a. Velit veritatis, modi minima deleniti culpa ratione facilis, nam distinctio, voluptatem rem cupiditate eaque corrupti.</p>
+    </div>
+  )
+}
